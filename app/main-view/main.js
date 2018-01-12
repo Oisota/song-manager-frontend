80b926afd2d1c1de12de5ee8220f5d1fb7091eeb
Derek Morey <dman6505@gmail.com>
2018-01-11 22:03:20 -0500

    Songs are now loaded via ajax

--- app/main-view/main.js
+++ app/main-view/main.js
@@ -6,6 +6,28 @@ module.exports = {
 		'song-table': SongTable,
 	},
 	data() {
-		return store.state;
+		return {
+			songs: []
+		}
+	},
+	created() {
+		this.loadSongs();
+	},
+	methods: {
+		loadSongs() {
+			const self = this;
+			$.ajax({
+				url: 'http://127.0.0.1:6505/api/songs',
+				method: 'GET',
+				timeout: 5000,
+				dataType: 'json'
+			})
+			.done((data, textStatus, jqXHR) => {
+				self.songs = data.songs;
+			})
+			.fail((jqXHR, textStatus, errorThrown) => {
+				console.log(errorThrown);
+			});
+		}
 	},
 };
