ed3a06eee441ce435bd58467357de990288e13d6
Derek Morey <dman6505@gmail.com>
2018-01-12 22:34:58 -0500

    Songs can now be edited on the page, but not saved

--- app/main-view/song-table/song-row/main.js
+++ app/main-view/song-table/song-row/main.js
@@ -8,5 +8,24 @@ module.exports = {
 			type: Number,
 			required: true,
 		}
+	},
+	data() {
+		return {
+			beingEdited: false,
+		}
+	},
+	methods: {
+		edit() {
+			this.beingEdited = true;
+			this.$nextTick(function () {
+				this.$el.querySelector('.song-name').children[0].focus();
+			});
+		},
+		update() {
+			this.beingEdited = false;
+		},
+		cancelUpdate() {
+			this.beingEdited = false;
+		}
 	}
 };
