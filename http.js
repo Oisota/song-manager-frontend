3ea04d0717e782c2972957816ff75715df9b3015
Derek Morey <derek.o.morey@gmail.com>
2018-04-24 19:48:48 -0400

    removed redundant axios interceptor

--- http.js
+++ http.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
 
-const state = require('./store/state');
-
 const http = axios.create({
 	baseURL: 'http://localhost:6505/api',
 	timeout: 5000,
@@ -9,13 +7,6 @@ const http = axios.create({
 	headers: {
 		'Content-Type': 'application/json',
 	}
-});
-
-http.interceptors.request.use(config => {
-	config.headers.authorization = `Bearer ${state.user.token}`;
-	return config;
-}, error => {
-	console.log(error);
 });
 
 module.exports = http;
