fcb0bb4c472cc6f304c32341d3e65dd991891b92
Derek Morey <derek.o.morey@gmail.com>
2018-05-23 20:07:33 -0400

    added register modal

--- app/pages/login-view/main.js
+++ app/pages/login-view/main.js
@@ -4,19 +4,45 @@ module.exports = {
 		return {
 			email: '',
 			password: '',
+			registerEmail: '',
+			registerPassword: '',
 		}
 	},
 	methods: {
-		async login() {
-			const resp = await this.$http.post('/auth/login', {
+		login() {
+			this.$http.post('/auth/login', {
 				email: this.email,
 				password: this.password,
+			}).then(resp => {
+				if (resp.status == 200) {
+					this.$store.commit('login', resp.data);
+					this.$router.push('/');
+				}
+			}).catch(err => {
+				console.log(err);
 			});
 
-			if (resp.status == 200) {
-				this.$store.commit('login', resp.data);
-				this.$router.push('/');
-			}
+		},
+		register() {
+			this.$http.post('/auth/register', {
+				email: this.registerEmail,
+				password: this.registerPassword,
+			}).then(resp => {
+				if (resp.status == 200) {
+					this.$store.commit('addAlert', {
+						title: 'Account Created',
+						message: 'An email will be sent once you are verified',
+						category: 'success'
+					});
+				}
+			}).catch(err => {
+				this.$store.commit('addAlert', {
+					title: 'Error',
+					message: 'Account not created',
+					category: 'danger'
+				});
+			});
+
 		}
 	}
 };
