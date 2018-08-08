5c1153242258fc2e8fd186ff58c6fcf5c11c761b
Derek Morey <derek.o.morey@gmail.com>
2018-08-07 22:09:06 -0400

    updated config settings

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 /* eslint no-process-env: 0 */
-exports.env = process.env.NODE_ENV;
-exports.historyMode = process.env.NODE_ENV === 'production' ? 'history' : 'hash';
+switch (process.env.NODE_ENV) {
+	case 'production':
+		exports.ENV = 'production';
+		exports.HISTORY_MODE = 'history';
+		exports.API_URL = window.location.origin + '/api';
+		break;
+	default:
+		exports.ENV = 'development';
+		exports.HISTORY_MODE = 'hash';
+		exports.API_URL = 'http://localhost:6505/api';
+}
