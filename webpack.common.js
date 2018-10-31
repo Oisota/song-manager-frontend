cadc00316ff04bc40ea5549b11b58d72fb6406d5
Derek Morey <derek.o.morey@gmail.com>
2018-10-30 22:45:38 -0400

    add eslint-loader to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -20,12 +20,17 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+						},
 					},
-				}
+					{
+						loader: 'eslint-loader'
+					}
+				]
 			},
 			{
 				test: /\.css$/,
