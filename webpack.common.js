62590e9fe88b21de33257fbe9beb2121d701df4a
Derek Morey <derek.o.morey@gmail.com>
2019-11-17 20:17:03 -0500

    Removed babel

--- webpack.common.js
+++ webpack.common.js
@@ -41,12 +41,6 @@ module.exports = {
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env'],
-						},
-					},
-					{
 						loader: 'eslint-loader'
 					}
 				]
